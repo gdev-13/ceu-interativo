75648d86205324b7a8aaac94ab4ef73c376708b3
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-05-13 12:54:17 -0300

    feat: captura a hora atual do sistema

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function carregar() {
     var mensagem = document.querySelector('#mensagem')
     var imagem = document.querySelector('#imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    mensagem.innerHTML = `Agora são ${hora} horas e ${minutos} minutos.`
 }
